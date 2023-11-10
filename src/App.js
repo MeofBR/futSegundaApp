5b6e480463151ecc2e0eb691b7adf0dd8a6a5039
Mauricio Filho <meof1993@gmail.com>
2023-11-10 17:10:37 -0300

    alteraçãoes

--- src/App.js
+++ src/App.js
@@ -13,31 +13,36 @@ function FutSegundaHome() {
   const [mediasTimes, setMediasTimes] = useState([]);
 
   const jogadores = [
-    { nome: 'Dudu', nota: 7.7, posicao: 'Lateral' },
+    { nome: 'Dudu', nota: 7.0, posicao: 'Lateral' },
     { nome: 'Maylon', nota: 8.03, posicao: 'Lateral' },
-    { nome: 'Thibes', nota: 5.5, posicao: 'Meia' },
-    { nome: 'João', nota: 6.07, posicao: 'Lateral' },
-    { nome: 'Sala', nota: 7, posicao: 'Atacante' },
-    { nome: 'Perdigão', nota: 8.5, posicao: 'Meia' },
+    { nome: 'Thibes', nota: 6.0, posicao: 'Meia' },
+    { nome: 'João', nota: 6.0, posicao: 'Lateral' },
+    { nome: 'Salazar', nota: 7, posicao: 'Atacante' },
+    { nome: 'Perdigão', nota: 8.3, posicao: 'Meia' },
     { nome: 'Vinicius', nota: 6.5, posicao: 'Zagueiro' },
-    { nome: 'Matheus', nota: 6.18, posicao: 'Meia' },
-    { nome: 'Eric', nota: 6.88, posicao: 'Atacante' },
+    { nome: 'Matheus', nota: 6.5, posicao: 'Zagueiro' },
+    { nome: 'Eric', nota: 7.5, posicao: 'Atacante' },
     { nome: 'Gabriel F', nota: 7.9, posicao: 'Zagueiro' },
-    { nome: 'Alexandre', nota: 6.58, posicao: 'Zagueiro' },
-    { nome: 'Cristian', nota: 8.5, posicao: 'Meia' },
+    { nome: 'Alexandre F', nota: 7.0, posicao: 'Meia' },
+    { nome: 'Cristian', nota: 8.7, posicao: 'Meia' },
     { nome: 'Danilo', nota: 6.13, posicao: 'Lateral' },
-    { nome: 'PB', nota: 8, posicao: 'Atacante' },
-    { nome: 'DG', nota: 5.2, posicao: 'Lateral' },
-    { nome: 'Diego', nota: 8.6, posicao: 'Atacante' },
-    { nome: 'Eduardo', nota: 7, posicao: 'Meio' },
+    { nome: 'Rodrigo PB', nota: 8.0, posicao: 'Atacante' },
+    { nome: 'DG', nota: 6.0, posicao: 'Lateral' },
+    { nome: 'Diego', nota: 8.5, posicao: 'Atacante' },
+    { nome: 'Eduardo', nota: 7.0, posicao: 'Meio' },
     { nome: 'Nivo', nota: 6.5, posicao: 'Zagueiro' },
     { nome: 'Diego Bodybuilder', nota: 7.5, posicao: 'Meio' },
-    { nome: 'Willian', nota: 7, posicao: 'Lateral' },
-    { nome: 'Joffe', nota: 8.6, posicao: 'Atacante' },
-    { nome: 'Dan ', nota: 7, posicao: 'Meia' },
-    { nome: 'Jose', nota: 6.5, posicao: 'Meia' },
+    { nome: 'Willian', nota: 7.0, posicao: 'Lateral' },
+    { nome: 'Joffe', nota: 8.0, posicao: 'Atacante' },
+    { nome: 'Dan ', nota: 7.0, posicao: 'Meia' },
+    { nome: 'José', nota: 6.0, posicao: 'Meia' },
     { nome: 'Marlon', nota: 7.0, posicao: 'Meio' },
     { nome: 'Ajair', nota: 7.25, posicao: 'Meia' },
+    { nome: 'Dan Grimm', nota: 7.0, posicao: 'Meia' },
+    { nome: 'Alexandre (Amigo Cristian)', nota: 8.5, posicao: 'Meia' },
+    { nome: 'Murilo (Amigo João)', nota: 6.7, posicao: 'Meia' },
+    { nome: 'Diego (Amigo Perdigão)', nota: 7.5, posicao: 'Meia' },
+    { nome: 'Tiago Almeida', nota: 7.8, posicao: 'Zagueiro' },
   ];
 
   function shuffleArray(array) {
@@ -109,6 +114,9 @@ function FutSegundaHome() {
 
   return (
     <ThemeProvider theme={theme}>
+      <Button onClick={handleTeamButton} className="sorterButton">
+        Sortear times
+      </Button>
       <div className="container-global">
         <div className="left">
           {loading && (
@@ -116,8 +124,6 @@ function FutSegundaHome() {
               <div className="spinner"></div>
             </div>
           )}
-          <Button onClick={handleTeamButton}>Sortear times</Button>
-
           <ul className="players-list">
             {jogadores.map((jogador) => (
               <li key={jogador.nome}>
@@ -126,8 +132,8 @@ function FutSegundaHome() {
                   onChange={() => handlePlayerCheckboxChange(jogador.nome)}
                 />
                 <span className="player-info">
-                  <strong>Nome:</strong> {jogador.nome},{' '}
-                  <strong>Posição:</strong> {jogador.posicao}
+                  <strong></strong> {jogador.nome}, <strong></strong>{' '}
+                  {jogador.posicao}
                 </span>
               </li>
             ))}
@@ -142,10 +148,10 @@ function FutSegundaHome() {
                   <strong>Time {index + 1}:</strong>
                   {time.map((jogador, jogadorIndex) => (
                     <p key={jogadorIndex}>
-                      Nome: {jogador.nome}, Posição: {jogador.posicao}
+                      - {jogador.nome}, {jogador.posicao}
                     </p>
                   ))}
-                  Média: {mediasTimes[index]}
+                  <h4>Média: {mediasTimes[index]}</h4>
                 </li>
               ))}
             </ul>
